4b1cfda1a300210acc0ab889a9a23b327cf1a19f
Sudhakar Patil <patilsudhakar.m@gmail.com>
2017-11-05 00:39:30 +0530

    Added entities

---
+++ MLMBiowillBusinessEntities/Master/Agent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLMBiowillBusinessEntities.Master
+{
+    public class Agent
+    {
+        public int Id { get; set; }
+        public string AgentCode { get; set; }
+        public string Salutation { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int BranchId { get; set; }
+        public int Sex  { get; set; }
+        public string PanNumber { get; set; }
+        public string AdharNumber { get; set; }
+        public string ImagePath { get; set; }
+        public bool Active { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
---
+++ MLMBiowillBusinessEntities/Master/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLMBiowillBusinessEntities.Master
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; } 
+        public string Password { get; set; }
+        public string LinkedWith { get; set; }
+        public bool Active { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
