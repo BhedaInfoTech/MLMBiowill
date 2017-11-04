f787ca5ebcf331a7022e48610b8ff78142a0073b
Sudhakar Patil <patilsudhakar.m@gmail.com>
2017-11-04 23:11:58 +0530

    Added subcategory entity.

--- MLMBiowillBusinessEntities/Master/Subcategory.cs
+++ MLMBiowillBusinessEntities/Master/Subcategory.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace MLMBiowillBusinessEntities.Master
 {
-    public class Category
+    public class Subcategory
     {
         public int Id { get; set; }
-        public string CategoryName { get; set; }
+        public string SubCategoryName { get; set; }
+        public int CategoryId { get; set; }
         public bool Active { get; set; }
         public int CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
-        public int UpdatedBy {get;set;}
+        public int UpdatedBy { get; set; }
         public DateTime UpdatedOn { get; set; }
     }
 }
