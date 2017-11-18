82fe608979db366d3f72159464a1550a07dffc5a
Sudhakar Patil <patilsudhakar.m@gmail.com>
2017-11-18 10:40:13 +0530

    Entity Changes

--- MLMBiowillBusinessEntities/Master/Subcategory.cs
+++ MLMBiowillBusinessEntities/Master/Subcategory.cs
@@ -13,8 +13,8 @@ namespace MLMBiowillBusinessEntities.Master
         public int CategoryId { get; set; }
         public bool Active { get; set; }
         public int CreatedBy { get; set; }
-        public DateTime CreatedOn { get; set; }
+        public DateTime CreatedDate { get; set; }
         public int UpdatedBy { get; set; }
-        public DateTime UpdatedOn { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
